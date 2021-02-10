c2d00e55684b0d01cc072ab10f1e3c5d1a823eb5
shubham <ska10@iitbbs.ac.in>
2021-02-11 04:29:01 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const hamburgerContainer = document.querySelector('.header__main-nav');
+
+const hamburger = document.querySelector('.header__main-nav--hamburger');
+
+const links = document.querySelectorAll('.header__main-nav--links li');
+
+hamburger.addEventListener('click' , (e)=>{
+    hamburgerContainer.classList.toggle('clicked');
+
+    links.forEach(link=>{
+        link.classList.toggle('fade');
+    })
+})
